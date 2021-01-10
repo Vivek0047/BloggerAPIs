05b695df9fcc037d07d96098dd45a95b6edae9ed
Vivek Kumar <vkumar@greenapplesolutions.com>
2021-01-10 20:11:02 +0530

    added required field in blogModel

--- BloggerAPIs/Models/BlogModel.cs
+++ BloggerAPIs/Models/BlogModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,9 @@ namespace BloggerAPIs.Models
 {
     public class BlogModel
     {
-        public int Id { get; set; }
-        public string Subject { get; set; }
-        public string Body { get; set; }
+        [Required] public int Id { get; set; }
+        [Required] public string Subject { get; set; }
+        [Required] public string Body { get; set; }
         public DateTime CreatedDateTime { get; set; }
     }
 }
